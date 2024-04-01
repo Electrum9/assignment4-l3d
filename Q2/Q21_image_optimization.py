d896a90bf5c72389ecab8599b2823f3e0a9ca7ac
VB <vikram.bhagavatula@gmail.com>
2024-03-31 20:08:31 -0400

    initial implementation of sds loss

--- Q2/Q21_image_optimization.py
+++ Q2/Q21_image_optimization.py
@@ -42,9 +42,9 @@ def optimize_an_image(
         
         ### YOUR CODE HERE ###
         if args.sds_guidance:
-            loss = 
+            loss = sds.sds_loss(latents, embeddings['default'], embeddings['uncond'], args.sds_guidance)
         else:
-            loss = 
+            loss = sds.sds_loss(latents, embeddings['default'], embeddings['uncond'])
 
         # Backward pass
         loss.backward()
--- Q2/SDS.py
+++ Q2/SDS.py
@@ -117,6 +117,7 @@ class SDS:
         imgs = (imgs * 255).round()  # [0, 1] => [0, 255]
         return imgs[0]
 
+    @torch.autocast(device_type='cuda')
     def sds_loss(
         self,
         latents,
@@ -148,22 +149,27 @@ class SDS:
             device=self.device,
         )
 
+        noise = torch.randn_like(latents, device=self.device)
+        latent_model_input = torch.sqrt(self.alphas[t]) * latents + torch.sqrt(1 - self.alphas[t]) * noise
+
         # predict the noise residual with unet, NO grad!
         with torch.no_grad():
             ### YOUR CODE HERE ###
- 
+            pred_noise = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
 
             if text_embeddings_uncond is not None and guidance_scale != 1:
+                pred_noise_text = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings_uncond).sample
                 ### YOUR CODE HERE ###
-                pass
- 
-
+                pred_noise = pred_noise + guidance_scale * (pred_noise_text - pred_noise)
 
         # Compute SDS loss
         w = 1 - self.alphas[t]
         ### YOUR CODE HERE ###
 
+        grad = w * (pred_noise - noise)
+        target = latents + grad
 
-        loss = 
+        mse = torch.nn.MSELoss()
+        loss = mse(latents, target)
 
         return loss
